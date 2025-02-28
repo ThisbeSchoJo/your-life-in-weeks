dc61f0fcb005d84b87d2aab7a83ad4a8d7780b36
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-02-28 15:00:58 -0500

    added init method for Week class in week.py and started a user_getter property

--- lib/models/week.py
+++ lib/models/week.py
@@ -1,1 +1,13 @@
 from models.__init__ import CONN, CURSOR
+
+class Week:
+    
+    def __init__(self, user, date, satisfaction_rating, comments):
+        self.user = user
+        self.date = date
+        self.satisfaction_rating = satisfaction_rating
+        self.comments = comments
+
+    @property
+    def user_getter():
+        pass
