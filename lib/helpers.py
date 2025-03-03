ae8f70f82d3245fc13330307073674ffdf159bfe
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-03-03 13:46:22 -0500

    imported users into helpers.py, updated menu choices from interacting with data to retrieving data in helpers.py

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,5 +1,6 @@
 # lib/helpers.py
 from models.week import Week
+from models.user import User
 from models.comment import Comment
 
 
@@ -10,29 +11,44 @@ def interact_with_weeks_data():
         if choice == "0":
             exit_program()
         elif choice == "1":
-            interact_with_weeks_data()
-        elif choice == "2":
-            interact_with_comments_data()
+            get_all_weeks()
+        # elif choice == "2":
+        #     interact_with_comments_data()
         else:
             print("Invalid choice")
-    print("Interacting with weeks data...")
-    input("--Enter any key to continue... --")
 
-def interact_with_comments_data():
+def get_all_weeks():
+    Week.get_all()
+
+    print("Here is the data for all weeks:\n")
+
+    for week in Week.all:
+        print(week)
+
+    input("\n--Enter any key to continue... --\n")
+
+def interact_with_users_data():
     while True:
         comment_menu()
         choice = input("> ")
         if choice == "0":
             exit_program()
         elif choice == "1":
-            interact_with_weeks_data()
-        elif choice == "2":
-            interact_with_comments_data()
+            get_all_users()
+        # elif choice == "2":
+        #     interact_with_comments_data()
         else:
             print("Invalid choice")
-    print("Interacting with comments data...")
-    input("--Enter any key to continue... --")
 
+def get_all_users():
+    User.get_all()
+
+    print("Here is the data for all users:\n")
+
+    for user in User.all:
+        print(user)
+
+    input("\n--Enter any key to continue... --\n")
 
 def exit_program():
     print("Goodbye!")
@@ -41,12 +57,12 @@ def exit_program():
 def week_menu():
     print("Please select an option:")
     print("0. Exit the program")
-    print("1. Interact with weeks data")
-    print("2. Interact with comments data")
+    print("1. Retrieve all weeks")
+    # print("2. Interact with comments data")
 
-def comment_menu():
+def user_menu():
     print("Please select an option:")
     print("0. Exit the program")
-    print("1. Interact with weeks data")
-    print("2. Interact with comments data")
+    print("1. Retrieve all users")
+    # print("2. Interact with comments data")
 
