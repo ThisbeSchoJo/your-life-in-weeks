9099a84f2608e1957582a6a7fc34ec1a6b6a9a62
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-03-03 13:38:42 -0500

    Imported User to seed.py and added seed data to seed.py

--- lib/seed.py
+++ lib/seed.py
@@ -1,15 +1,21 @@
 from models.week import Week
+from models.user import User
 from models.comment import Comment
 
 
 def seed_database():
     Week.drop_table()
+    User.drop_table()
     Week.create_table()
+    User.create_table()
 
     Week.create("Thisbe", 12025, 1, "bad time")
     Week.create("Thisbe", 22025, 9, "started classes!")
     Week.create("Thisbe", 32025, 8, "Enjoying all the change!")
 
+    User.create("Thisbe", "1995-10-04")
+    User.create("Sydney", "1998-02-14")
+
     print("Weeks successfully seeded!")
 
-seed_database
+seed_database()
