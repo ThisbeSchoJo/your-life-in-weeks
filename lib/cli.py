07fc3ec0dd6f2c32fa27fb112150f7b310d452f7
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-03-05 20:36:31 -0500

    Updated welcome message in cli.py and imported Console to add styling

--- lib/cli.py
+++ lib/cli.py
@@ -1,4 +1,6 @@
 # lib/cli.py
+from colorama import Fore, Style
+from rich.console import Console
 
 from helpers import (
     exit_program,
@@ -13,8 +15,16 @@ from models.week import Week
 def main():
     User.create_table()
     Week.create_table()
+    console = Console()
 
-    print("\nWelcome to Your Life in Weeks!\n")
+    # print(Style.BRIGHT + Fore.GREEN + "\nWelcome to Your Life in Weeks!\n" + Style.RESET_ALL)
+    console.print("[bold cyan]\nWelcome to Your Life in Weeks![/bold cyan]\n")
+    console.print("[italic cyan]It kind of feels like our lives are made up of a countless number of weeks.[italic cyan]\n")
+    console.print("[italic cyan]We imagine we will walk our dogs a hundred more times, we agonize over something embarrassing we said, \nwe pull our hair out over our crying toddler, we tell ourselves we'll finally ask out the barista next time, \nwe forget to return our sister's phone call, our plans never make it out of the friend's group chat, \nwe make shallow small talk and forget to listen when others respond, we tell ourselves we will go skydiving *someday*, \nwe hold off saying 'I love you' because it is too soon, we get annoyed with our mother's nagging, \nwe think 'how's work?' is enough with our father, we stay at a job that makes us miserable for fear of change. [italic cyan]\n")
+    console.print("[italic cyan]We let a million moments pass us by unnoticed, unappreciated, or untouched, because we think we have unlimited time.[italic cyan]\n")
+    console.print("[italic cyan]A passing week feels like a drop in the bucket of our time. [italic cyan]\n")
+    console.print("[italic cyan]But truly, we each only get a finite, countable number of weeks... [italic cyan]\n\n")
+    console.print("[italic cyan]This interactive CLI tool lays out your life before you—every week you've lived and every week you have left.[italic cyan]\n")
     while True:
         
         menu()
